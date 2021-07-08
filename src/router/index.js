4ec0cfeba1084d1e37039276d0898c0ec75650d4
Viktor <viktorkatz619@gmail.com>
2021-07-08 14:07:37 +0200

    Before i18n

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Kiosk from '../views/Kiosk.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/kiosk',
+        name: 'Kiosk',
+        component: Kiosk
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/Kiosk.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="kioskFrame">
+        <div class="center">
+            <img alt="Logo" class="logo" src="../assets/bikeLogo.png">
+            <h1>Prislonite karticu kako biste se ulogovali</h1>
+            <p>Pritisnite x da biste simulirali prislanjanje kartice</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Kiosk',
+        components: {
+
+        }
+    }
+</script>
+
+<style>
+    .center {
+        margin-top:20%;
+    }
+
+    img.logo {
+        width: 300px;
+        height: auto;
+    }
+
+    .kioskFrame {
+        width: 800px;
+        height: 600px;
+        border: 3px black solid;
+        margin: auto;
+        background-color: #9AF09D;
+    }
+
+</style>
